78179692ebb24eef8f484166b923fce1d6f3c6f3
Tie Ma <tie.ma@anz.com>
2023-07-19 11:11:13 +1000

    add hello service

---
+++ cmd/hello/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	grpchealth "github.com/bufbuild/connect-grpchealth-go"
+	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/marty-anz/hello-grpcgo-render/pkg/hello/service/v1alpha1/v1alpha1connect"
+	"github.com/marty-anz/hello-grpcgo-render/pkg/service/hello/v1alpha1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	mux := http.NewServeMux()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	serviceNames := []string{v1alpha1connect.HelloName}
+
+	reflector := grpcreflect.NewStaticReflector(serviceNames...)
+	checker := grpchealth.NewStaticChecker(serviceNames...)
+
+	mux.Handle(grpcreflect.NewHandlerV1(reflector))
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(reflector))
+	mux.Handle(grpchealth.NewHandler(checker))
+
+	service := &v1alpha1.Service{}
+
+	servicePath, serviceHandler := v1alpha1connect.NewHelloHandler(
+		service,
+	)
+
+	mux.Handle(servicePath, serviceHandler)
+
+	server := &http.Server{
+		Addr:              ":" + port,
+		Handler:           h2c.NewHandler(mux, &http2.Server{}),
+		ReadHeaderTimeout: time.Second,
+		ReadTimeout:       5 * time.Minute,
+		WriteTimeout:      5 * time.Minute,
+		MaxHeaderBytes:    8 * 1024, // 8KiB
+	}
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Default().Println(fmt.Sprintf("HTTP listen and serve: %v", err))
+	}
+}
---
+++ pkg/service/hello/v1alpha1/service.go
@@ -0,0 +1,18 @@
+package v1alpha1
+
+import (
+	"context"
+
+	connect_go "github.com/bufbuild/connect-go"
+
+	"github.com/marty-anz/hello-grpcgo-render/pkg/hello/service/v1alpha1"
+)
+
+type Service struct {
+}
+
+func (s *Service) SayHello(context.Context, *connect_go.Request[v1alpha1.SayHelloRequest]) (*connect_go.Response[v1alpha1.SayHelloResponse], error) {
+	return connect_go.NewResponse(
+		&v1alpha1.SayHelloResponse{Message: "hello world"},
+	), nil
+}
